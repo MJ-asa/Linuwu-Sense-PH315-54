8329bc2df3ad442832f1878e2fb31576c713a423
Ash <88560705+MJ-asa@users.noreply.github.com>
2025-09-25 04:04:32 -0500

    Update linuwu_sense.c to include PH315-54 support
    not completely sure if the enabled features will work for everyone

--- src/linuwu_sense.c
+++ src/linuwu_sense.c
@@ -471,6 +471,14 @@ enum acer_wmi_predator_v4_oc {
      .turbo = 1,
      .cpu_fans = 1,
      .gpu_fans = 1,
+};
+
+ static struct quirk_entry quirk_acer_predator_ph315_54 = {
+     .predator_v4 = 1,
+     .four_zone_kb = 1,
+     .turbo = 1,
+     .cpu_fans = 1,
+     .gpu_fans = 1,
  };
  
  static struct quirk_entry quirk_acer_predator_phn16_71 = {
@@ -731,6 +739,15 @@ enum acer_wmi_predator_v4_oc {
              DMI_MATCH(DMI_PRODUCT_NAME, "Predator PH315-53"),
          },
          .driver_data = &quirk_acer_predator_ph315_53,
+     },
+     {
+         .callback = dmi_matched,
+         .ident = "Acer Predator PH315-54",
+         .matches = {
+             DMI_MATCH(DMI_SYS_VENDOR, "Acer"),
+             DMI_MATCH(DMI_PRODUCT_NAME, "Predator PH315-54"),
+         },
+         .driver_data = &quirk_acer_predator_ph315_54,
      },
      {
          .callback = dmi_matched,
